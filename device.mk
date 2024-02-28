ba389fda33cbccf3c6343015e1d4e99f7b038dfc
ghazzor <127341071+ghazzor@users.noreply.github.com>
2024-02-28 18:11:22 +0530

    Remove Sme Useless Stuff FOr twrp

--- device.mk
+++ device.mk
@@ -4,41 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
-
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Partitions
-PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Product characteristics
-PRODUCT_CHARACTERISTICS := phone
-
-# Rootdir
-PRODUCT_PACKAGES += \
-    gps.sh \
-    enable_test_mode.sh \
-    install-recovery.sh \
-    mx_log_collection.sh \
-    mx_logger_dump.sh \
-    scsc_get_platform_info.sh \
 
 PRODUCT_PACKAGES += \
     fstab.exynos9611 \
@@ -54,6 +29,3 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
-
-# Inherit the proprietary files
-$(call inherit-product, vendor/samsung/m21/m21-vendor.mk)
